55ac7f7bbec39ceaeb6758acfbaec259735b1b8a
0x766e <53742374+0x766e@users.noreply.github.com>
2021-02-09 09:50:04 +0000

    Add some placeholders

--- src/api/api.js
+++ src/api/api.js
@@ -11,4 +11,26 @@ export default {
       return Promise.resolve(edenic.insertVowels(word));
     },
   },
+
+  shift: {
+    shiftAll(word) {
+      return Promise.resolve(['shift1', 'shift2', 'shift3']);
+    },
+  },
+
+  metathesis: {
+    scramble(word) {
+      return Promise.resolve(['scramble1', 'scramble2', 'scramble3']);
+    },
+  },
+
+  nasalization: {
+    insertAll(word) {
+      return Promise.resolve([
+        'nasalization1',
+        'nasalization2',
+        'nasalization3',
+      ]);
+    },
+  },
 };
