a28e641ea457f3dcd72eaa4ea3a079cd5eb709b3
0x766e <53742374+0x766e@users.noreply.github.com>
2021-02-11 14:55:51 +0000

    Finish wiring

--- src/modules/Result.js
+++ src/modules/Result.js
@@ -1,11 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import NavigationBar from '../components/NavigationBar';
-import Container from 'react-bootstrap/Container';
-import Tabs from 'react-bootstrap/Tabs';
-import Tab from 'react-bootstrap/Tab';
+import Pagination from 'react-bootstrap/Pagination';
 import Table from 'react-bootstrap/Table';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 import ApiContext from '../contexts/ApiContext';
 import WordContext from '../contexts/WordContext';
 
@@ -13,68 +11,109 @@ export default function Result(props) {
   const api = useContext(ApiContext);
   const currentWord = useContext(WordContext);
 
-  const [selectedTab, setSelectedTab] = useState('transcription');
-  const [transcription, setTranscription] = useState([]);
-  const [selectedTranscription, setSelectedTranscription] = useState('');
-  const [vocalization, setVocalization] = useState([]);
+  const [wordAnalysis, setWordAnalysis] = useState([]);
+  const [filterCriteria, setFilterCriteria] = useState({
+    vowels: false,
+    shifts: false,
+    scramble: false,
+    nasalization: false,
+  });
 
   useEffect(() => {
-    api.transcription.fromHebrew(currentWord).then((result) => {
-      setTranscription(result);
-      setSelectedTranscription(result.length > 0 ? result[0] : '');
+    api.analyze(currentWord, filterCriteria).then((result) => {
+      setWordAnalysis(result);
     });
-  }, [currentWord, api.transcription]);
-
-  useEffect(() => {
-    if (selectedTranscription) {
-      api.vowels.insertAll(selectedTranscription).then((result) => {
-        setVocalization(result);
-      });
-    }
-  }, [selectedTranscription, api.vowels]);
+  }, [filterCriteria, currentWord, api]);
 
   return (
     <>
       <NavigationBar searchTerm={currentWord} onSearch={props.onSearch} />
-      <Container className="mt-5">
-        <Tabs
-          defaultActiveKey="profile"
-          activeKey={selectedTab}
-          onSelect={(tab) => setSelectedTab(tab)}
-        >
-          <Tab eventKey="transcription" title="Transcription">
-            <Card>
-              <Card.Body>
-                <Card.Title>{currentWord}</Card.Title>
-                <ListGroup>
-                  {transcription.map((t) => (
-                    <ListGroup.Item
-                      action
-                      active={t === selectedTranscription}
-                      onClick={() => setSelectedTranscription(t)}
-                    >
-                      {t.join('')}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-                {vocalization.length > 0 && (
-                  <Table striped bordered hover>
-                    <thead>
-                      <th>Vowels</th>
-                    </thead>
-                    <tbody>
-                      {vocalization.map((t) => (
-                        <tr>
-                          <td>{t.join('')}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </Table>
-                )}
-              </Card.Body>
-            </Card>
-          </Tab>
-        </Tabs>
+      <Container>
+        <h1>Root: {currentWord}</h1>
+        <Form inline>
+          <Form.Group>
+            <Form.Control type="text" placeholder="Filter" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Check
+              type="checkbox"
+              label="Vowels"
+              checked={filterCriteria.vowels}
+              onChange={(e) =>
+                setFilterCriteria({
+                  ...filterCriteria,
+                  vowels: e.target.checked,
+                })
+              }
+            />
+            <Form.Check
+              type="checkbox"
+              label="Shifts"
+              checked={filterCriteria.shifts}
+              onChange={(e) =>
+                setFilterCriteria({
+                  ...filterCriteria,
+                  shifts: e.target.checked,
+                })
+              }
+            />
+            <Form.Check
+              type="checkbox"
+              label="Metathesis"
+              checked={filterCriteria.scramble}
+              onChange={(e) =>
+                setFilterCriteria({
+                  ...filterCriteria,
+                  scramble: e.target.checked,
+                })
+              }
+            />
+            <Form.Check
+              type="checkbox"
+              label="Nasalization"
+              checked={filterCriteria.nasalization}
+              onChange={(e) =>
+                setFilterCriteria({
+                  ...filterCriteria,
+                  nasalization: e.target.checked,
+                })
+              }
+            />
+          </Form.Group>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Word</th>
+              <th>Transformation</th>
+            </tr>
+          </thead>
+          <tbody>
+            {wordAnalysis.map(([word, transformations]) => (
+              <tr>
+                <td>{word}</td>
+                <td>{transformations}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Item>{1}</Pagination.Item>
+          <Pagination.Ellipsis />
+
+          <Pagination.Item>{10}</Pagination.Item>
+          <Pagination.Item>{11}</Pagination.Item>
+          <Pagination.Item active>{12}</Pagination.Item>
+          <Pagination.Item>{13}</Pagination.Item>
+          <Pagination.Item disabled>{14}</Pagination.Item>
+
+          <Pagination.Ellipsis />
+          <Pagination.Item>{20}</Pagination.Item>
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
       </Container>
     </>
   );
